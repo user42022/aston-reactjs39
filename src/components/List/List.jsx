fdb2624ca253741883239e3d23412aefef691819
user42022 <andrekizilov@yandex.ru>
2024-05-15 22:50:47 +0300

    fix: replace List class component with function component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,97 +1,37 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Form from '../Form/Form'
 import ListItem from "./ListItem/ListItem";
 import './List.css'
 
 
-export default class List extends React.Component {
+export default function List() {
 
-  constructor(props) {
-    super(props);
+  console.log('List rendered')
 
-    this.state = {
-      names: []
-    };
-    this.addName = this.addName.bind(this);
-    this.removeName = this.removeName.bind(this);
+  const [names, setNames] = useState([])
+  useEffect(() => {
+    console.log(`List did update`);
+    return () => console.log(`List unmounted`);
+  })
 
-    console.log('List constructed')
-  }
-
-  addName(name) {
-    this.setState((state) => {
-      return { names: state.names.concat(name) }
+  const addName = (name) => {
+    setNames((prevNames) => {
+      return prevNames.concat(name)
     })
   }
 
-  removeName(idToRemove) {
-    this.setState((state) => {
-      return { names: state.names.filter(({ id }) => id !== idToRemove) }
+  const removeName = useCallback((idToRemove) => {
+    setNames((prevNames) => {
+      return prevNames.filter(({ id }) => id !== idToRemove)
     })
-  }
+  }, [setNames])
 
-  static getDerivedStateFromProps() {
-    console.log('List getDerivedStateFromProps')
-    return null
-  }
-
-  static getDerivedStateFromError() {
-    console.log('List getDerivedStateFromError')
-  }
-
-  getSnapshotBeforeUpdate() {
-    console.log('List getSnapshotBeforeUpdate')
-    return null
-  }
-
-  componentDidMount() {
-    console.log('List mounted')
-  }
-
-  componentDidUpdate() {
-    console.log('List updated')
-  }
-
-  componentWillUnmount() {
-    console.log('List will remove')
-  }
-
-  componentDidCatch() {
-    console.log('List catched error')
-  }
-
-  shouldComponentUpdate() {
-    console.log('List should update?')
-    return true
-  }
-
-  // UNSAFE methods --- start
-  UNSAFE_componentWillMount() {
-    console.log('List will mount')
-  }
-
-  UNSAFE_componentWillReceiveProps() {
-    console.log('List will recieve props')
-  }
-
-  UNSAFE_componentWillUpdate() {
-    console.log('List will update')
-  }
-  // UNSAFE methods --- end
-
-
-  render() {
-    console.log('List rendered')
-
-    return (
-      <div className="list">
-        <Form addName={this.addName} />
-        <ul>
-          {this.state.names.map((name) => <ListItem {...name} key={name.id} removeName={this.removeName} />)}
-        </ul>
-      </div>
-    )
-  }
-
-
+  return (
+    <div className="list">
+      <Form addName={addName} />
+      <ul>
+        {names.map((name) => <ListItem {...name} key={name.id} removeName={removeName} />)}
+      </ul>
+    </div>
+  )
 }
